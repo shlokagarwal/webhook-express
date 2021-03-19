9747f7548bfbd0150a287bdfdc75ee5a2b4fb60d
Agarwalla <Shlok_Agarwalla@Dell.com>
2021-03-19 10:24:00 +0530

    updated

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
 const app = express()
 const port = 3000
+app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+app.post('/post-test', (req, res) => {
+  console.log(req.body);
+  res.send('Hello World!');
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
